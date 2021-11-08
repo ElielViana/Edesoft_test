ac60a2fbaca49c69ddc6054ebfd0bd1f981bdb5c
ElielViana <elieloliveira163@gmail.com>
2021-11-08 03:29:45 -0300

    removes unused import

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
-import 'package:intl/intl.dart';
 import 'package:task_list/pages/home/home_controller.dart';
 import 'package:task_list/pages/task_add/task_add_page.dart';
 import 'package:task_list/stores/tasks_store.dart';
